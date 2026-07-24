923f03d8562fb27a650c7c045fc5fb437a62e4ba
baseballproject1 <ryuhj0829@gmail.com>
2026-07-24 12:00:16 +0900

    Update save.js

--- system/save.js
+++ system/save.js
@@ -1,6 +1,6 @@
 // ============================
 // system/save.js
-// 저장 시스템
+// 저장 / 초기화 시스템
 // ============================
 
 
@@ -30,22 +30,26 @@ function saveGame(){
 
 
 
+
 // 불러오기
 
 function loadGame(){
 
 
-    let data =
+    let save =
+
     localStorage.getItem(
         SAVE_KEY
     );
 
 
-    if(data){
+
+    if(save){
 
 
         gameData =
-        JSON.parse(data);
+
+        JSON.parse(save);
 
 
     }
@@ -57,16 +61,91 @@ function loadGame(){
 
 
 
-// 데이터 초기화
 
-function resetGame(){
+// 저장 데이터 확인
+
+function checkSave(){
 
 
-    localStorage.removeItem(
+    let save =
+
+    localStorage.getItem(
         SAVE_KEY
     );
 
 
+
+    if(save){
+
+
+        alert(
+            "💾 저장 데이터 있음"
+        );
+
+
+    }
+
+    else{
+
+
+        alert(
+            "💾 저장 데이터 없음"
+        );
+
+
+    }
+
+
+}
+
+
+
+
+
+
+// 게임 초기화
+
+function resetGame(){
+
+
+
+    let check =
+
+    confirm(
+
+        "⚠️ 게임을 초기화하시겠습니까?\n\n"
+        +
+        "모든 선수, 자금, 아이템이 삭제됩니다."
+
+    );
+
+
+
+    if(!check)
+
+        return;
+
+
+
+
+
+    localStorage.removeItem(
+
+        SAVE_KEY
+
+    );
+
+
+
+    alert(
+
+        "게임이 초기화되었습니다."
+
+    );
+
+
+
     location.reload();
+
 
 }
